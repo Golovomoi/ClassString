93ab81acff92289f29a7028060f9b0255cd5959b
Игорь Кузнецов <golovomoi@gmail.com>
2023-10-18 03:24:57 +1000

    add sorting

--- ClassString/ClassString.cpp
+++ ClassString/ClassString.cpp
@@ -1,20 +1,37 @@
 ﻿#include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
 #include "headers/AwsomeString.h"
 
 int main()
 {
-    AwsomeString* st = new AwsomeString("qqwwee");
-    AwsomeString sat = "asd";
-    AwsomeString sit = "123";
-    sat = "ggffdd";
-    AwsomeString sot(sat);
-    sit = sot;
-    AwsomeString moved = std::move(sit);
-    moved += sat;
-    moved += "333";
-    AwsomeString conStr = "123" + AwsomeString("456");
-    AwsomeString conStr1 = AwsomeString("ab") + "bc";
-    AwsomeString conStr2 = conStr + conStr1;
-    std::cout << "Strings!\n";
+    std::cout << "Введите число строк: " << std::endl;
+    int n;
+    std::cin >> n;
+    std::cout << "введите " << n << " строк разделяя их клавишей enter" << std::endl;
+    
+    std::vector<AwsomeString> ansVector;
+
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    for (auto i = 0; i < n; i++) {
+        AwsomeString tmp;
+        
+        std::cin >> tmp;
+        ansVector.emplace_back(std::move(tmp));
+    }
+
+    std::sort(ansVector.begin(), ansVector.end(), [](AwsomeString& first, AwsomeString& second) -> bool
+        {
+            return !(first < second);
+        }
+    );
+    std::cout << "строки отсортированные в порядке, обратном лексикографическому: " << std::endl;
+
+    for (auto i : ansVector)
+    {
+        std::cout << i << std::endl;
+    }
+
 }
